95f757e9a2268528867ac5abd2f5da8aba59f29a
LeMoustachu <hugoberthetrambaud@outlook.com>
2022-12-18 20:34:23 +0100

    spinning cube uing lines only!!

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
+camera.position.set( 0, 0, 50 );
 camera.lookAt( 0, 0, 0 );
 
 const scene = new THREE.Scene();
@@ -12,11 +12,34 @@ const scene = new THREE.Scene();
 const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
 
 const points = [];
-points.push( new THREE.Vector3(  0, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 10, 0 ) );
-points.push( new THREE.Vector3( 10, 10, 0 ) );
-points.push( new THREE.Vector3( 10, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 0, 0 ) );
+points.push( new THREE.Vector3( -5,-5,-5 ) );
+points.push( new THREE.Vector3(-5, 5,-5 ) );
+points.push( new THREE.Vector3( 5, 5,-5 ) );
+points.push( new THREE.Vector3( 5,-5,-5 ) );
+points.push( new THREE.Vector3(-5,-5,-5 ) );
+
+points.push( new THREE.Vector3(-5,-5, 5 ) );
+points.push( new THREE.Vector3( 5,-5, 5 ) );
+points.push( new THREE.Vector3( 5,-5,-5 ) );
+
+points.push( new THREE.Vector3( 5, 5,-5 ) );
+points.push( new THREE.Vector3( 5, 5, 5 ) );
+points.push( new THREE.Vector3( 5,-5, 5 ) );
+
+points.push( new THREE.Vector3(-5,-5, 5 ) );
+
+points.push( new THREE.Vector3(-5,-5,-5 ) );
+points.push( new THREE.Vector3(-5, 5,-5 ) );
+points.push( new THREE.Vector3(-5, 5, 5 ) );
+points.push( new THREE.Vector3(-5,-5, 5 ) );
+
+
+points.push( new THREE.Vector3(-5, 5, 5 ) );
+points.push( new THREE.Vector3(-5, 5,-5 ) );
+points.push( new THREE.Vector3( 5, 5,-5 ) );
+points.push( new THREE.Vector3( 5, 5, 5 ) );
+points.push( new THREE.Vector3(-5, 5, 5 ) );
+
 
 const geometry = new THREE.BufferGeometry().setFromPoints( points );
 
@@ -27,8 +50,8 @@ renderer.render( scene, camera );
 
 function animate() {
 	requestAnimationFrame( animate );
-    line.rotation.y += 0.01;
-    line.rotation.x += 0.05;
+    line.rotation.y += 0.02;
+    line.rotation.x = 0.5;
 	renderer.render( scene, camera );
 }
 animate();
