731c6f900fade56d5c8727d67fa4209941dd5afe
LeMoustachu <hugoberthetrambaud@outlook.com>
2022-12-18 20:15:39 +0100

    draw lines...

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 10, 10, 10 );
-const material = new THREE.MeshBasicMaterial( { color: 0xBBF0FD } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-camera.position.z = 30;
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
 
-function animate() {
-    requestAnimationFrame( animate );
-    renderer.render( scene, camera );
-    cube.rotation.z += 0.01;
-    cube.rotation.y += 0.001;
-    cube.rotation.x += 0.01;
-    console.log("hahaha");
-}
-animate();
+const scene = new THREE.Scene();
+
+//create a blue LineBasicMaterial
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+const line = new THREE.Line( geometry, material );
+
+scene.add( line );
+renderer.render( scene, camera );
